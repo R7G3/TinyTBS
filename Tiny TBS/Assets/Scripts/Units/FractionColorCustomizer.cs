058a4c81bedccdd61367d1a40d0e52a3a8d03129
Ivan Fateev <truefiresnake@gmail.com>
2023-03-10 20:43:02 +0300

    Fix: occupy building does not update color #37 (#40)
    Building now updates its color
    Unit is moving to the building before capturing it

--- Tiny TBS/Assets/Scripts/Units/FractionColorCustomizer.cs
+++ Tiny TBS/Assets/Scripts/Units/FractionColorCustomizer.cs
@@ -9,14 +9,31 @@ namespace Assets.Scripts.Units
     {
         [SerializeField] private RendererMaterial[] _renderers;
         [SerializeField] private FractionsConfig _config;
+        private IGameplayObject _gameplayObject;
+        private string _currentFraction;
 
-        public void Init(IFraction fraction)
+        public void Init(IGameplayObject gameplayObject)
         {
-            var fractionConfig = _config.fractions.First(i => i.id == fraction.Id);
+            _gameplayObject = gameplayObject;
+        }
+
+        private void Update()
+        {
+            if (_gameplayObject != null && _currentFraction != _gameplayObject.Fraction.Id)
+            {
+                UpdateFraction();
+            }
+        }
+
+        private void UpdateFraction()
+        {
+            var fractionConfig = _config.fractions.First(i => i.id == _gameplayObject.Fraction.Id);
             foreach (var rendererMaterial in _renderers)
             {
                  rendererMaterial.renderer.materials[rendererMaterial.materialIndex].color = fractionConfig.color;
             }
+
+            _currentFraction = _gameplayObject.Fraction.Id;
         }
 
         [Serializable]
