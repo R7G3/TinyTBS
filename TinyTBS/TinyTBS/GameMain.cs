9f35049edf1f2c20da4fca9907eebb16ca6edf83
Vadim Trofimov <tulahas@yandex.ru>
2026-03-22 23:46:43 +0300

    Add line ending consistency

--- TinyTBS/TinyTBS/GameMain.cs
+++ TinyTBS/TinyTBS/GameMain.cs
@@ -32,7 +32,8 @@ namespace TinyTBS
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed
+                || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
