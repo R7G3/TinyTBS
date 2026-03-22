0395b2a19496e736f34cd2b6e155110fd9ca3869
Vadim Trofimov <42806631+R7G3@users.noreply.github.com>
2026-03-22 19:36:59 +0300

    Init MG project; Update gitignore; (#82)

---
+++ TinyTBS/TinyTBS/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TinyTBS
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using var game = new TinyTBS.GameMain();
+            game.Run();
+        }
+    }
+}
